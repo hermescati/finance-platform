dd87974029c9d56a7792ba0b4da67beb0ddb79b2
Hermes Cati <79720372+hermescati@users.noreply.github.com>
2023-02-17 19:40:47 +0200

    Deleting ViewModel Factories to use Delegates (#2)

---
+++ Expensier.WPF/State/Navigators/DelegateRenavigator.cs
@@ -0,0 +1,27 @@
+﻿using Expensier.WPF.ViewModels;
+using Expensier.WPF.ViewModels.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expensier.WPF.State.Navigators
+{
+    public class DelegateRenavigator<TViewModel> : IRenavigator where TViewModel : ViewModelBase
+    {
+        private readonly INavigator _navigator;
+        private readonly CreateViewModel<TViewModel> _createViewModel;
+
+        public DelegateRenavigator(INavigator navigator, CreateViewModel<TViewModel> createViewModel)
+        {
+            _navigator = navigator;
+            _createViewModel = createViewModel;
+        }
+
+        public void Renavigate()
+        {
+            _navigator.CurrentViewModel = _createViewModel();
+        }
+    }
+}
---
+++ Expensier.WPF/ViewModels/Factories/ExpensierViewModelFactory.cs
@@ -0,0 +1,46 @@
+﻿using Expensier.WPF.State.Navigators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expensier.WPF.ViewModels.Factories
+{
+    public class ExpensierViewModelFactory : IExpensierViewModelFactory
+    {
+        private readonly CreateViewModel<DashboardViewModel> _createDashboardViewModel;
+        private readonly CreateViewModel<ExpensesViewModel> _createExpenseViewModel;
+        private readonly CreateViewModel<WalletViewModel> _createWalletViewModel;
+        private readonly CreateViewModel<LoginViewModel> _createLoginViewModel;
+
+        public ExpensierViewModelFactory(
+            CreateViewModel<DashboardViewModel> createDashboardViewModel, 
+            CreateViewModel<ExpensesViewModel> createExpenseViewModel, 
+            CreateViewModel<WalletViewModel> createWalletViewModel, 
+            CreateViewModel<LoginViewModel> createLoginViewModel)
+        {
+            _createDashboardViewModel = createDashboardViewModel;
+            _createExpenseViewModel = createExpenseViewModel;
+            _createWalletViewModel = createWalletViewModel;
+            _createLoginViewModel = createLoginViewModel;
+        }
+
+        public ViewModelBase CreateViewModel(ViewType viewType)
+        {
+            switch (viewType)
+            {
+                case ViewType.Dashboard:
+                    return _createDashboardViewModel();
+                case ViewType.Expenses:
+                    return _createExpenseViewModel();
+                case ViewType.Wallet:
+                    return _createWalletViewModel();
+                case ViewType.Login:
+                    return _createLoginViewModel();
+                default:
+                    throw new ArgumentException("The View Type does not have a ViewModel.", "viewType");
+            }
+        }
+    }
+}
