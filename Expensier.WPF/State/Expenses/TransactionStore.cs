d10ec95e5779edbf321117a29241af7e714285ea
Hermes <hermescati99@gmail.com>
2023-02-19 18:13:28 +0200

    Retrieve user transaction data from database

---
+++ Expensier.WPF/State/Expenses/TransactionStore.cs
@@ -0,0 +1,31 @@
+﻿using Expensier.Domain.Models;
+using Expensier.WPF.State.Accounts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expensier.WPF.State.Expenses
+{
+    public class TransactionStore
+    {
+        private readonly AccountStore _accountStore;
+
+        public IEnumerable<Transaction> TransactionList => _accountStore.CurrentAccount?.TransactionList ?? new List<Transaction>();
+
+        public event Action StateChanged;
+
+        public TransactionStore(AccountStore accountStore)
+        {
+            _accountStore = accountStore;
+
+            _accountStore.StateChanged += OnStateChanged;
+        }
+
+        private void OnStateChanged()
+        {
+            StateChanged?.Invoke();
+        }
+    }
+}
---
+++ Expensier.WPF/ViewModels/TransactionViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Expensier.WPF.State.Expenses;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expensier.WPF.ViewModels
+{
+    public class TransactionViewModel : ViewModelBase
+    {
+        private readonly TransactionStore _transactionStore;
+        private readonly ObservableCollection<TransactionDataModel> _transactions;
+
+        public IEnumerable<TransactionDataModel> Transactions => _transactions;
+
+
+        public TransactionViewModel(TransactionStore transactionStore)
+        {
+            _transactionStore = transactionStore;
+
+            _transactions = new ObservableCollection<TransactionDataModel>();
+            _transactionStore.StateChanged += Transaction_StateChanged;
+
+            ResetTransactions();
+        }
+
+        private void ResetTransactions()
+        {
+            IEnumerable<TransactionDataModel> transactionViewModels = _transactionStore.TransactionList
+                .Select(t => new TransactionDataModel(t.Transaction_Name, t.Process_Date, t.Amount, t.Is_Credit));
+
+            _transactions.Clear();
+            foreach (TransactionDataModel dataModel in transactionViewModels)
+            {
+                _transactions.Add(dataModel);
+            }
+        }
+
+        private void Transaction_StateChanged()
+        {
+            ResetTransactions();
+        }
+    }
+}
