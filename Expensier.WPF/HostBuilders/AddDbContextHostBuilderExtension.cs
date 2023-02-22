8870fc05b2d5d884829bcf5212b49b5f0fee5e30
Hermes Cati <79720372+hermescati@users.noreply.github.com>
2023-02-22 22:47:32 +0200

    Cleaning up dependency injection container (#11)

---
+++ Expensier.WPF/HostBuilders/AddDbContextHostBuilderExtension.cs
@@ -0,0 +1,30 @@
+﻿using Expensier.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expensier.WPF.HostBuilders
+{
+    public static class AddDbContextHostBuilderExtension
+    {
+        public static IHostBuilder AddDbContext(this IHostBuilder host)
+        {
+            host.ConfigureServices((context, services) =>
+            {
+                string connectionString = context.Configuration.GetConnectionString("sqlite");
+                Action<DbContextOptionsBuilder> configureDbContext = con => con.UseSqlite(connectionString);
+
+                services.AddDbContext<ExpensierDbContext>(configureDbContext);
+                services.AddSingleton<ExpensierDbContextFactory>(new ExpensierDbContextFactory(configureDbContext));
+            });
+
+            return host;
+        }
+    }
+}
---
+++ Expensier.WPF/HostBuilders/AddViewHostBuilderExtension.cs
@@ -0,0 +1,24 @@
+﻿using Expensier.WPF.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expensier.WPF.HostBuilders
+{
+    public static class AddViewHostBuilderExtension
+    {
+        public static IHostBuilder AddViews(this IHostBuilder host)
+        {
+            host.ConfigureServices(services =>
+            {
+                services.AddSingleton(s => new MainView(s.GetRequiredService<MainViewModel>()));
+            });
+
+            return host;
+        }
+    }
+}
