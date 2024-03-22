5549ece033a042e569bf565ea106d2fbc8b91056
hermescati <hermescati99@gmail.com>
2024-03-22 23:44:02 +0100

    fix: search bar will check upon pressing enter

--- Expensier.WPF/Controls/SearchBar.xaml.cs
+++ Expensier.WPF/Controls/SearchBar.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.IdentityModel.Tokens;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Expensier.WPF.Controls
 {
@@ -19,43 +11,66 @@ namespace Expensier.WPF.Controls
     {
         private bool _isSearching;
 
+
         public static readonly DependencyProperty TextProperty =
-            DependencyProperty.Register("Text", typeof(string), typeof(SearchBar),
-                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
-                    TextPropertyChanged, null, false, UpdateSourceTrigger.PropertyChanged));
+            DependencyProperty.Register( "Text", typeof( string ), typeof( SearchBar ),
+                new FrameworkPropertyMetadata( string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    TextPropertyChanged, null, false, UpdateSourceTrigger.PropertyChanged ) );
 
 
-        private static void TextPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void TextPropertyChanged( DependencyObject d, DependencyPropertyChangedEventArgs e )
         {
-            if (d is SearchBar searchBar)
+            if ( d is SearchBar searchBar )
             {
                 searchBar.UpdateContent();
             }
         }
 
+
         public string Text
         {
-            get { return (string)GetValue(TextProperty); }
-            set { SetValue(TextProperty, value); }
+            get { return (string) GetValue( TextProperty ); }
+            set { SetValue( TextProperty, value ); }
         }
+
 
         public SearchBar()
         {
             InitializeComponent();
         }
 
-        private void SeachBar_PasswordChanged(object sender, RoutedEventArgs e)
+
+        private void UpdateText()
         {
             _isSearching = true;
             Text = searchBar.Text;
             _isSearching = false;
         }
 
+
         private void UpdateContent()
         {
-            if (!_isSearching)
+            if ( !_isSearching )
             {
                 searchBar.Text = Text;
+            }
+        }
+
+
+        private void searchBar_KeyUp( object sender, KeyEventArgs e )
+        {
+            if ( e.Key == Key.Enter )
+            {
+                UpdateText();
+            }
+        }
+
+
+        private void searchBar_TextChanged( object sender, TextChangedEventArgs e )
+        {
+            if ( searchBar.Text.IsNullOrEmpty() )
+            {
+                UpdateText();
             }
         }
     }
