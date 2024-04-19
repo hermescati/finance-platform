86a89e9b04468c4ab48e10cc88f5e410b793ea29
hermescati <hermescati@arkit.ch>
2024-04-19 17:01:50 +0200

    refactor: updated asset list component

--- Expensier.WPF/Converters/NullToVisibilityConverter.cs
+++ Expensier.WPF/Converters/NullToVisibilityConverter.cs
@@ -3,18 +3,41 @@ using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
 
+
 namespace Expensier.WPF.Converters
 {
     public class NullToVisibilityConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
         {
-            return value != null ? Visibility.Visible : Visibility.Collapsed;
+            if ( value == null )
+                return Visibility.Collapsed;
+
+            if ( value is bool boolValue && !boolValue )
+                return Visibility.Collapsed;
+
+            if ( value is string stringValue && string.IsNullOrEmpty(stringValue) )
+                return Visibility.Collapsed;
+
+            if ( IsNumericType( value ) && ( double ) value == 0 )
+                return Visibility.Collapsed;
+
+            return Visibility.Visible;
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+
+        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
         {
             throw new NotImplementedException();
+        }
+
+
+        private bool IsNumericType( object value )
+        {
+            return value is int ||
+                   value is double ||
+                   value is decimal ||
+                   value is float;
         }
     }
 }
